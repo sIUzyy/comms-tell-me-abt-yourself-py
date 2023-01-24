37c5bd367bb33432b91e835f8f238a17ad51c0ca
Justin Peligro <122611993+sIUzyy@users.noreply.github.com>
2023-01-24 22:44:39 +0800

    Add files via upload

---
+++ program.py
@@ -0,0 +1,23 @@
+print("\n********************************\n")
+print("Tell me something about yourself")
+print("\n********************************\n")
+
+#Ask user information
+FullName= input("\n Full name: ")
+NickName = input("\n Nick name: ")
+Age = input("\n Age: ")
+ZodiacSign = input("\n Zodiac Sign: ")
+Weight = input("\n Weight(kg): ")
+Height = input("\n Height(ft): ")
+Color = input("\n Color: ")
+Song = input("\n Song: ")
+Movie = input("\n Movie: ")
+Hobby = input("\n Hobby: ")
+print ("\n\n")
+
+#Print user information
+print ("Hi! My name is", FullName, ", and my friends call me", NickName,".")
+print ("I'm", Age, "years old, my zodiac sign is", ZodiacSign,".")
+print ("My weight is", Weight, "and", Height, "tall.")
+print ("I love", Color, "My favorite song is", Song, "and I like the movie", Movie,".")
+print ("My hobby is", Hobby, ".That's all :)")
